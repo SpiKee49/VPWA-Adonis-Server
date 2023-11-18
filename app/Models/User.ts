e48b8cc4c2785ce18dcaa40283cabc37793c701b
Silvia <xkuchtovas@stuba.sk>
2023-11-18 13:06:00 +0100

    added user_name to database

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -18,6 +18,9 @@ export default class User extends BaseModel {
   public id: number
 
   @column()
+  public userName: string
+
+  @column()
   public email: string
 
   @column({ serializeAs: null })
